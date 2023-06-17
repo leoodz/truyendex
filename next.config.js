5071e57f5974653f1c158b2372790e43e1afcc41
Mai Truong <nhoxhungvnq2@gmail.com>
2023-06-18 01:00:00 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,13 +10,23 @@ const nextConfig = {
     },
 }
 
-async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'https://cors.zenno.moe/:path*',
-          },
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "https://cors.zenno.moe/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
-      },
+      }
+    ]
+  }
+};
+
 
 module.exports = nextConfig
